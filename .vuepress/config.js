76bac1a156b685f43d905edc14939f42b3db0840
宿命 <45293856+sumingyd@users.noreply.github.com>
2023-02-07 03:43:46 +0800

    fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -39,86 +39,89 @@ module.exports = {
         lastUpdated: true,
         lastUpdated: '上次更新',
         logo: '/homepage.png',
-        nav: [{
-            text: '指南菜单',
-            items: [
-                {
-                    text: 'OpenCore安装',
-                    link: 'https://sumingyd.github.io/OpenCore-Install-Guide/'
-                },
-                {
-                    text: 'OpenCore安装后',
-                    link: 'https://sumingyd.github.io/OpenCore-Post-Install/'
-                },
-                {
-                    text: 'OpenCore多重引导',
-                    link: 'https://sumingyd.github.io/OpenCore-Multiboot/'
-                },
-                {
-                    text: '开始使用ACPI',
-                    link: 'https://sumingyd.github.io/Getting-Started-With-ACPI/'
-                },
-                {
-                    text: '无线购买指南',
-                    link: 'https://sumingyd.github.io/Wireless-Buyers-Guide/'
-                },
-                {
-                    text: '显卡购买指南',
-                    link: 'https://sumingyd.github.io/GPU-Buyers-Guide/'
-                },
-                {
-                    text: '避免购买指南',
-                    link: 'https://sumingyd.github.io/Anti-Hackintosh-Buyers-Guide/'
-                },
-            ]
-        },
+        nav: [
+            {
+                text: '指南菜单',
+                items: [
+                    {
+                        text: 'OpenCore安装',
+                        link: 'https://sumingyd.github.io/OpenCore-Install-Guide/'
+                    },
+                    {
+                        text: 'OpenCore安装后',
+                        link: 'https://sumingyd.github.io/OpenCore-Post-Install/'
+                    },
+                    {
+                        text: 'OpenCore多重引导',
+                        link: 'https://sumingyd.github.io/OpenCore-Multiboot/'
+                    },
+                    {
+                        text: '开始使用ACPI',
+                        link: 'https://sumingyd.github.io/Getting-Started-With-ACPI/'
+                    },
+                    {
+                        text: '无线购买指南',
+                        link: 'https://sumingyd.github.io/Wireless-Buyers-Guide/'
+                    },
+                    {
+                        text: '显卡购买指南',
+                        link: 'https://sumingyd.github.io/GPU-Buyers-Guide/'
+                    },
+                    {
+                        text: '避免购买指南',
+                        link: 'https://sumingyd.github.io/Anti-Hackintosh-Buyers-Guide/'
+                    },
+                ]
+            },
+            { text: 'QQ群', link: 'https://jq.qq.com/?_wv=1027&k=liYHt5VH' },
         ],
-        sidebar: [{
-            title: '介绍',
-            collapsable: false,
-            sidebarDepth: 1,
-            children: [
-                '',
-            ]
+        sidebar: [
+            {
+                title: '介绍',
+                collapsable: false,
+                sidebarDepth: 1,
+                children: [
+                    '',
+                ]
 
-        },
-        {
-            title: '应该避免什么',
-            collapsable: false,
-            sidebarDepth: 2,
-            children: [
-                'CPU',
-                'GPU',
-				'Motherboard',
-				'Storage',
-				'RAM',
-				'Cooler',
-				'Networking',
-				'Wireless',
-				'PSU',
-				'Case',
-				'ThermalPaste',
-            ]
-        },
-    ],
-},
+            },
+            {
+                title: '应该避免什么',
+                collapsable: false,
+                sidebarDepth: 2,
+                children: [
+                    'CPU',
+                    'GPU',
+                    'Motherboard',
+                    'Storage',
+                    'RAM',
+                    'Cooler',
+                    'Networking',
+                    'Wireless',
+                    'PSU',
+                    'Case',
+                    'ThermalPaste',
+                ]
+            },
+        ],
+    },
 
     plugins: [
         ['@vuepress/back-to-top', true],//开启右下角返回顶层图标
         ['@vuepress/nprogress', true],//这个插件将会在你切换页面的时候，在顶部显示进度条。
         ['vuepress-plugin-smooth-scroll', true],//在你的 VuePress 站点中使用平滑滚动。
         ['vuepress-plugin-fulltext-search', true],//基于 Headers 的搜索插件
-        ['@vuepress/medium-zoom',{
-                selector: ".theme-succinct-content :not(a) > img",
-                options: {
-                    background: 'var(--bodyBgColor)'
-                }
+        ['@vuepress/medium-zoom', {
+            selector: ".theme-succinct-content :not(a) > img",
+            options: {
+                background: 'var(--bodyBgColor)'
             }
+        }
         ],//这个插件将会使你的图片支持点击缩放。
         ['@vuepress/active-header-links', {
             sidebarLinkSelector: '.sidebar-link',
             headerAnchorSelector: '.header-anchor'
-            }
+        }
         ],//页面滚动时自动激活侧边栏链接的插件
     ]
 }
